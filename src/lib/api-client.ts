20007939b55f2419f2449dcadb57a7584aef62df
Ting Lu <tinglu30@gmail.com>
2025-08-03 15:28:58 -0400

    config - setup

---
+++ src/lib/api-client.ts
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+export const api = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost:8000',
+});
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    config.headers['Authorization'] = `Bearer ${token}`;
+  }
+  return config;
+});
+
+api.interceptors.response.use(
+  (response) => response.data,
+  (error: unknown) => {
+    if (axios.isAxiosError(error)) {
+      console.error('API Error:', error.message);
+    } else {
+      console.error('Unexpected Error:', error);
+    }
+    console.error('API Response Error:', error);
+    return Promise.reject(error);
+  }
+);
